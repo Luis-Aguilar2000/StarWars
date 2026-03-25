a9bf815b842d3cc3d5673ddd4c60cbcd80730a65
herz-05 <hm202401@upes.edu.sv>
2026-03-25 13:11:53 -0600

    clases intermedias

---
+++ StarWars/Models/PersonaPelicula.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarWars.Models
+{
+    public class PersonaPelicula
+    {
+        public int PersonaId { get; set; }
+        public int PeliculaId { get; set; }
+    }
+}
---
+++ StarWars/Models/PersonaTransporte.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarWars.Models
+{
+    public class PersonaTransporte
+    {
+        public int PersonaId { get; set; }
+        public int TransporteId { get; set; }
+    }
+}
